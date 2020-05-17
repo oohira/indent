347117b61d2bd48882a2a289d89b9f13a9f0eb4e
oohira <oohira.dev@gmail.com>
2020-05-17 16:42:53 +0900

    feat: Support a file argument (but only 1 file)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"os"
 )
@@ -10,7 +11,18 @@ func main() {
 	spaces := flag.Int("n", 4, "number of spaces")
 	flag.Parse()
 
-	err := indent(os.Stdin, os.Stdout, *spaces)
+	var r io.Reader
+	if flag.NArg() == 0 {
+		r = os.Stdin
+	} else {
+		file, err := os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		r = file
+	}
+	err := indent(r, os.Stdout, *spaces)
 	if err != nil {
 		log.Fatal(err)
 	}
